5fa44f525a8a09711f024c385ace11c563350ca0
scottg <scottghome@scottgs-MBP.lan>
2024-05-19 10:19:31 -0500

    adding pretty blue background

--- game.py
+++ game.py
@@ -23,7 +23,8 @@ class Game:
             'grass': load_images('tiles/grass'),
             'large_decor': load_images('tiles/large_decor'),
             'stone': load_images('tiles/stone'),
-            'player': load_image('entities/player.png')
+            'player': load_image('entities/player.png'),
+            'background': load_image('background.png'),
         }
 
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
@@ -37,7 +38,8 @@ class Game:
         run = True
         while run:
 
-            self.display.fill((14, 219, 248))
+            #self.display.fill((14, 219, 248))
+            self.display.blit(self.assets['background'], (0, 0))
 
             self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
             self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30

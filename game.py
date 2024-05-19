1d50a8043fe3d57202caadc59740603814fca600
scottg <scottghome@scottgs-MBP.lan>
2024-05-19 09:44:14 -0500

    update

--- game.py
+++ game.py
@@ -30,6 +30,8 @@ class Game:
 
         self.tilemap = Tilemap(self, tile_size=16)
 
+        self.scroll = [0, 0]
+
 
     def run(self):
         run = True
@@ -37,11 +39,16 @@ class Game:
 
             self.display.fill((14, 219, 248))
 
-            self.tilemap.render(self.display)
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.tilemap.render(self.display, offset=render_scroll)
 
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
 
-            self.player.render(self.display)
+            self.player.render(self.display, offset=render_scroll)
 
             print(self.tilemap.physics_rects_around(self.player.pos))
 

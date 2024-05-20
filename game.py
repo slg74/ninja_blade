075e3b60823dd41a8ec908dde2a343d9d0db6744
scottg <scottghome@scottgs-MBP.lan>
2024-05-20 07:07:54 -0500

    remove comments, reformat long line statements.

--- game.py
+++ game.py
@@ -11,7 +11,7 @@ class Game:
     def __init__(self):
         pygame.init()
 
-        pygame.display.set_caption('Ninja Blade')
+        pygame.display.set_caption("Ninja Blade")
         self.screen = pygame.display.set_mode((640, 480))
         self.display = pygame.Surface((320, 240))
 
@@ -20,39 +20,48 @@ class Game:
         self.movement = [False, False]
 
         self.assets = {
-            'decor': load_images('tiles/decor'),
-            'grass': load_images('tiles/grass'),
-            'large_decor': load_images('tiles/large_decor'),
-            'stone': load_images('tiles/stone'),
-            'player': load_image('entities/player.png'),
-            'background': load_image('background.png'),
-            'clouds': load_images('clouds'),
-            'player/idle': Animation(load_images('entities/player/idle'), image_duration=6),
-            'player/run': Animation(load_images('entities/player/run'), image_duration=4),
-            'player/jump': Animation(load_images('entities/player/jump')),
-            'player/slide': Animation(load_images('entities/player/slide')),
-            'player/wall_slide': Animation(load_images('entities/player/wall_slide'))
+            "decor": load_images("tiles/decor"),
+            "grass": load_images("tiles/grass"),
+            "large_decor": load_images("tiles/large_decor"),
+            "stone": load_images("tiles/stone"),
+            "player": load_image("entities/player.png"),
+            "background": load_image("background.png"),
+            "clouds": load_images("clouds"),
+            "player/idle": Animation(
+                load_images("entities/player/idle"), image_duration=6
+            ),
+            "player/run": Animation(
+                load_images("entities/player/run"), image_duration=4
+            ),
+            "player/jump": Animation(load_images("entities/player/jump")),
+            "player/slide": Animation(load_images("entities/player/slide")),
+            "player/wall_slide": Animation(load_images("entities/player/wall_slide")),
         }
 
+        self.clouds = Clouds(self.assets["clouds"], count=16)
 
-        self.clouds = Clouds(self.assets['clouds'], count=16)
-
-        #self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
         self.player = Player(self, (50, 50), (8, 15))
 
         self.tilemap = Tilemap(self, tile_size=16)
 
         self.scroll = [0, 0]
 
-
     def run(self):
         run = True
         while run:
 
-            self.display.blit(self.assets['background'], (0, 0))
+            self.display.blit(self.assets["background"], (0, 0))
 
-            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
-            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+            self.scroll[0] += (
+                self.player.rect().centerx
+                - self.display.get_width() / 2
+                - self.scroll[0]
+            ) / 30
+            self.scroll[1] += (
+                self.player.rect().centery
+                - self.display.get_height() / 2
+                - self.scroll[1]
+            ) / 30
 
             render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
 
@@ -64,8 +73,6 @@ class Game:
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
 
             self.player.render(self.display, offset=render_scroll)
-
-            print(self.tilemap.physics_rects_around(self.player.pos))
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -86,7 +93,9 @@ class Game:
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
 
-            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+            self.screen.blit(
+                pygame.transform.scale(self.display, self.screen.get_size()), (0, 0)
+            )
             pygame.display.update()
             self.clock.tick(60)
 
--- scripts/entities.py
+++ scripts/entities.py
@@ -80,20 +80,19 @@ class PhysicsEntity:
 
 class Player(PhysicsEntity):
     def __init__(self, game, pos, size):
-        super().__init__(game, 'player', pos, size)
+        super().__init__(game, "player", pos, size)
         self.air_time = 0
 
     def update(self, tilemap, movement=(0, 0)):
         super().update(tilemap, movement=movement)
 
         self.air_time += 1
-        if self.collisions['down']:
+        if self.collisions["down"]:
             self.air_time = 0
 
         if self.air_time > 4:
-            self.set_action('jump')
+            self.set_action("jump")
         elif movement[0] != 0:
-            self.set_action('run')
+            self.set_action("run")
         else:
-            self.set_action('idle')
-
+            self.set_action("idle")
